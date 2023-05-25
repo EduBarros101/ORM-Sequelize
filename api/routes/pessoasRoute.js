5e2c40c2826a29ef1b17e883f5fd3e888ef8ebd8
Eduardo Barros <edu.barros101@gmail.com>
2023-05-25 17:18:30 -0300

    Add: contagem de turmas lotadas.

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -13,6 +13,11 @@ router
     PessoaController.pegaUmaMatricula
   )
   .get('/pessoas/:estudanteId/matricula', PessoaController.pegaMatriculas)
+  .get(
+    '/pessoas/matricula/:turmaId/confirmadas',
+    PessoaController.pegaMatriculasPorTurma
+  )
+  .get('/pessoas/matricula/lotada', PessoaController.pegaTurmasLotadas)
   .post('/pessoas', PessoaController.criaPessoa)
   .post('/pessoas/:estudanteId/matricula', PessoaController.criaMatricula)
   .post('/pessoas/:id/restaura', PessoaController.restauraPessoa)
